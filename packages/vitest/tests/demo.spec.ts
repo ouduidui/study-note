f4da237bfabcf4476a7ae74dca3af3d973a864e3
oushihao <oushihao97@163.com>
2022-03-21 14:09:38 +0800

    feat: study vitest describe

--- packages/vitest/tests/demo.spec.ts
+++
@@ -1,8 +0,0 @@
-import { assert, describe, test } from 'vitest'
-
-describe.skip('skipped suite', () => {
-  test('sqrt', () => {
-    // Suite skipped, no error
-    assert.equal(Math.sqrt(4), 3)
-  })
-})
